52ccf466571b79aac2352db2e6aebe384474bc01
adamsmith <adamrsmith10@gmail.com>
2025-11-19 23:36:16 -0800

    Added some documentation and links to some of the sources that I used to build the program.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,29 @@
+/* This is a program that uses some of the concepts from Linear Algebra
+ * to rotate shapes on the screen using OpenGL. Vertex and indices data is loaded
+ * from a file in a folder called "Data" in the "Resources" folder. The shaders are
+ * also stored there. I created a Shape class which encapsulates reading the data from those
+ * files and sending the data to buffers on the graphics card.
+ *
+ * I cannot take full credit for the ShaderClass as it was created from a combination of a tutorial I was watching
+ * and a online textbook. Both are linked below and I would not have been able to learn as much as I have about
+ * how to use OpenGL without these resources. The code in the ShaderClass is just another encapsulation of OpenGL code
+ * for readability purposes. I am also using a free library called IMGUI for the GUI which I will link below. I found there
+ * library to be incredibly easy to use which is impressive because I have done some GUI programming in the past, and it gets
+ * messy easily. Those classes are available in the project and I understand what they do. I want to learn
+ * more in the future as I really enjoyed graphics programming. This project really challenged me and I feel I really grew as
+ * a programmer while creating a complex project like this. I have made one other software in the past in my free time that I would
+ * classify as an application and while making this I felt like I grew even more in my ability to build complex applications.
+ *
+ * https://learnopengl.com/
+ * https://www.youtube.com/watch?v=XpBGwZNyUh0&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=2
+ * https://github.com/ocornut/imgui
+ * https://www.opengl.org/
+ *
+ * Author: Adam Smith
+ * Date: 11/19/2025
+ * Professor: Dr. Amy Yielding
+ * Course: MTH341 Linear Algebra
+
 /* GUI */
 #include "../Header Files/imgui/imgui.h" // The GUI library itself
 #include "../Header Files/imgui/imgui_impl_glfw.h" // Need this since I am using GLFW for my window
@@ -57,34 +83,41 @@ static float rotateY = 0.0f;
 static float rotateZ = 0.0f;
 static float fov = 45.0f;
 static glm::vec3 translation = glm::vec3(0.0f, 0.0f, 0.0f);
-static glm::vec3 scale = glm::vec3(0.6f, 0.6f, 0.6f);
+static glm::vec3 scale = glm::vec3(0.7f, 0.7f, 0.7f);
 static glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, -2.2f);
 
+// A list of shapes. C++ is wierd, and it uses vectors as the name of its version of a list data structure.
+// "vec's" and "mat's" are the names of the actual mathematical matrices included in the OpenGL Mathematics Library.
 static std::vector<Shape> shapes;
+// The index of the shape that is currently being drawn to the screen.
 static int currentShapeIndex = 0;
-
+// I multiply this by my degrees of rotation according to the windows time when the shape is auto rotating so it spins faster every second.
 static float rotationSpeed = 1.5f;
+// These are some booleans I have set to determine the default state of certain parameters in the program.
 static bool autoRotate = false;
 static bool isWireframe = false;
 static bool faceCulling = true;
 static bool antialiasing = true;
 
-
 int main()
 {
+   // initialize glfw
    glfwInit();
+   // Use window hints to tell glfw some information
    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
    glfwWindowHint(GLFW_SAMPLES, 4);
-
+   // create the window
    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL", NULL, NULL);
+   // Handle errors
    if (window == NULL)
    {
       std::cout << "Failed to create GLFW window" << std::endl;
       glfwTerminate();
       return FAILURE;
    }
+   // make the context for the window
    glfwMakeContextCurrent(window);
    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
 
@@ -93,14 +126,20 @@ int main()
       std::cout << "Failed to initialize GLAD" << std::endl;
       return FAILURE;
    }
+   // Create the shader program given the glsl and fragment shader files
    Shader shaderProgram(vertexShaderPath, fragmentShaderPath);
+   // initialize the GUI
    initializeGUI(window);
 
    while(!glfwWindowShouldClose(window))
    {
+      // fill the shapes vector with all my shapes
       constructShapes(shapes);
+      // generate the matrices and send them to the vertex shader
       generateMatrices(shaderProgram);
+      // process any keyboard input
       processInput(window);
+      // create the frame for the GUI
       createGUIFrame();
 
       // clear the window color every frame
@@ -110,19 +149,25 @@ int main()
       // clear the color and depth buffers before each render iteration
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+      // activate the shader program
       shaderProgram.Activate();
+      // draw the whichever shape is at the currently selected index
       shapes[currentShapeIndex].Draw();
+      // create the GUI
       createGUI();
 
       glfwSwapBuffers(window);
       glfwPollEvents();
    }
    deleteGUI();
+   // delete the shader program
    shaderProgram.Delete();
+   // delete all the shapes
    for (int i = 0; i < shapes.size(); i++)
    {
       shapes[i].Delete();
    }
+   // terminate the window
    glfwTerminate();
    return SUCCESS;
 }
@@ -132,7 +177,9 @@ int main()
  */
 void constructShapes(const std::vector<Shape>&)
 {
+   // reserve space for 6 shapes
    shapes.reserve(6);
+   // add the shapes to the vector
    shapes.emplace_back(octagonVerticesPath,octagonIndicesPath);
    shapes.emplace_back(cubeVerticesPath, cubeIndicesPath);
    shapes.emplace_back(pyramidVerticesPath, pyramidIndicesPath);
@@ -140,16 +187,20 @@ void constructShapes(const std::vector<Shape>&)
    shapes.emplace_back(icosahedronVerticesPath, icosahedronIndicesPath);
    shapes.emplace_back(dodecahedronVerticesPath,dodecahedronIndicesPath);
 }
-
+/*
+ * A function that I use to create my model, view, and projection matrix
+ * before sending the data to the vertex shader.
+ */
 void generateMatrices(Shader shaderProgram)
 {
+   // the model matrix.. which is a combination of scale, translation, and rotation matrices
    glm::mat4 modelMatrix = glm::mat4(1.0f);
    modelMatrix = glm::scale(modelMatrix, scale);
    modelMatrix = glm::translate(modelMatrix, translation);
    // check if auto rotate has been enabled and change which matrix transformations are used based on the boolean auto rotate
    if (autoRotate)
    {
-      modelMatrix = glm::rotate(modelMatrix, (float)glfwGetTime() * glm::radians(50.0f)*rotationSpeed, glm::vec3(0.5f, 1.0f, 0.0f));
+      modelMatrix = glm::rotate(modelMatrix, (float)glfwGetTime() * glm::radians(50.0f)*rotationSpeed, glm::vec3(1.0f, 1.0f, 0.0f));
    }
    else
    {
@@ -157,13 +208,14 @@ void generateMatrices(Shader shaderProgram)
       modelMatrix = glm::rotate(modelMatrix, glm::radians(rotateY),glm::vec3(0.0f,1.0f,0.0f));
       modelMatrix = glm::rotate(modelMatrix, glm::radians(rotateZ),glm::vec3(0.0f, 0.0f,1.0f));
    }
+   // view matrix
    glm::mat4 viewMatrix = glm::mat4(1.0);
    viewMatrix = glm::translate(viewMatrix, cameraPosition);
-
+   // projection matrix
    glm::mat4 projectionMatrix = glm::mat4(1.0);
    projectionMatrix = glm::perspective(glm::radians(fov), (GLfloat)WINDOW_WIDTH/(GLfloat)WINDOW_HEIGHT, 0.1f, 100.0f);
 
-   // send it all to the vertex shader
+   // Get the locations of each uniform and send them to the vertex shader
    int modelLocation = glGetUniformLocation(shaderProgram.ID, "modelMatrix");
    glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(modelMatrix));
    int viewLocation = glGetUniformLocation(shaderProgram.ID, "viewMatrix");
@@ -172,6 +224,9 @@ void generateMatrices(Shader shaderProgram)
    glUniformMatrix4fv(projectionLocation, 1, GL_FALSE,glm::value_ptr(projectionMatrix));
 }
 
+/*
+ * A function to hold all the boilerplate GUI initialization code
+ */
 void initializeGUI(GLFWwindow *window)
 {
    IMGUI_CHECKVERSION();
@@ -181,7 +236,9 @@ void initializeGUI(GLFWwindow *window)
    ImGui_ImplGlfw_InitForOpenGL(window, true);
    ImGui_ImplOpenGL3_Init("#version 460");
 }
-
+/*
+ * Creates the GUI Frame
+ */
 void createGUIFrame()
 {
    ImGui_ImplOpenGL3_NewFrame();
@@ -344,4 +401,3 @@ void frameBufferSizeCallback(GLFWwindow* window, int width, int height)
 {
    glViewport(0, 0, width, height);
 }
-
